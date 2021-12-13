535c164bda92b506192bcb9d9cef902d466da2bf
style <xl88@bu.edu>
2021-12-12 19:11:46 -0500

    Add change interest& exchange rate

--- src/AdminPage.java
+++ src/AdminPage.java
@@ -6,36 +6,43 @@ import java.util.List;
 
 public class AdminPage implements ActionListener {
     private JButton changeInterestButton;
+    private JButton changeExchangeButton;
     private JButton dailyReportButton;
-//    private JButton openAccountButton;
+    private JButton importButton;
     private JButton logOutButton;
     private JButton stockButton;
     private JLabel infolabel;
     private JFrame frame;
     private String uid;
+    private String stockPath = "/src/System Data/publicStock.txt";
+    private String interestPath = "/src/System Data/Interest.txt";
+    private String ratePath =  "/src/System Data/Exchange.txt";
 
     public AdminPage(String userID) {
         JLabel adminLabel = new JLabel(userID);
         uid = userID;
         changeInterestButton = new JButton("Change Interest");
-//        openAccountButton = new JButton("Open Account");
+        changeExchangeButton = new JButton("Change Exchange Rate");
+        importButton = new JButton("Import Data");
         dailyReportButton = new JButton("Daily Report");
-        stockButton = new JButton("Stock");
+        stockButton = new JButton("Stock Simulate");
         logOutButton = new JButton("Log Out");
         infolabel = new JLabel("");
 
         adminLabel.setBounds(200,10,180,30);
         adminLabel.setFont(new Font(null,Font.ITALIC,35));
-        changeInterestButton.setBounds(20,110,150,25);
-//        openAccountButton.setBounds(20,110,150,25);
-        dailyReportButton.setBounds(20,150,150,25);
-        stockButton.setBounds(20,190,150,25);
-        logOutButton.setBounds(20,230,150,25);
-        infolabel.setBounds(20,270,200,25);
+        importButton.setBounds(20,70,200,25);
+        changeInterestButton.setBounds(20,110,200,25);
+        changeExchangeButton.setBounds(20,150,200,25);
+        stockButton.setBounds(20,190,200,25);
+        dailyReportButton.setBounds(20,230,200,25);
+        logOutButton.setBounds(20,270,200,25);
+        infolabel.setBounds(20,310,200,25);
 
 
         changeInterestButton.addActionListener(this);
-//        openAccountButton.addActionListener(this);
+        changeExchangeButton.addActionListener(this);
+        importButton.addActionListener(this);
         dailyReportButton.addActionListener(this);
         stockButton.addActionListener(this);
         logOutButton.addActionListener(this);
@@ -44,10 +51,11 @@ public class AdminPage implements ActionListener {
         frame.add(adminLabel);
         frame.add(infolabel);
         frame.add(changeInterestButton);
-//        frame.add(openAccountButton);
+        frame.add(importButton);
         frame.add(dailyReportButton);
         frame.add(stockButton);
         frame.add(logOutButton);
+        frame.add(changeExchangeButton);
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400, 420);
@@ -59,20 +67,21 @@ public class AdminPage implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         if(e.getSource()==changeInterestButton) {
-            //TODO change Interest
-            frame.dispose();
+            changePrice(2);
         }
-//        if(e.getSource()==openAccountButton) {
-//            //TODO open Account
-//            frame.dispose();
-//
-//        }
+        if(e.getSource()==importButton) {
+           importData();
+        }
         if(e.getSource()==dailyReportButton) {
             frame.dispose();
         }
         if(e.getSource()==stockButton) {
-            changePrice();
+            changePrice(1);
         }
+        if(e.getSource()==changeExchangeButton) {
+            changePrice(3);
+        }
+
         if(e.getSource()==logOutButton) {
             //TODO
             frame.dispose();
@@ -81,18 +90,55 @@ public class AdminPage implements ActionListener {
 
     }
 
-    private void changePrice() {
+    private void importData() {
+        fileEditor.writeFile(stockPath,new String[]{"SONY","1131.96","YEN"});
+        fileEditor.writeFile(stockPath,new String[]{"NIO","374.60","RMB"});
+        fileEditor.writeFile(stockPath,new String[]{"APPL","190.73","USD"});
+
+        fileEditor.writeFile(ratePath,new String[]{"USD","1"});
+        fileEditor.writeFile(ratePath,new String[]{"RMB","0.16"});
+        fileEditor.writeFile(ratePath,new String[]{"YEN","0.0088"});
+
+        fileEditor.writeFile(interestPath,new String[]{"0.02"});
+    }
+
+    private void changePrice(int numType) {
         //change stock price
-        String filename = "/src/System Data/publicStock.txt";
-        List<String[]> stockList = fileEditor.fileRead(filename);
+        String filename,type;
+        if(numType==1) {
+            filename = stockPath;
+            type = "Stock";
+        }
+        else if(numType==2) {
+            filename = interestPath;
+            type = "Interest";
+        }else {
+            filename = ratePath;
+            type = "Exchange Rate";
+        }
+
+        List<String[]> infoList = fileEditor.fileRead(filename);
         fileEditor.clearFile(filename);
         //message display
-        for (String[]stockinfo : stockList){
-            double price = Double.valueOf(stockinfo[1]);
-            price *= 0.9+0.2*Math.random();
-            stockinfo[1] = String.format("%.2f",price);
-            fileEditor.writeFile(filename,stockinfo);
+        for (String[]info : infoList){
+
+            if(numType==1) {
+                double price = Double.valueOf(info[1]);
+                price *= 0.9 + 0.2 * Math.random();
+                info[1] = String.format("%.2f",price);
+            }
+            else if(numType==2)  {
+                double price = Double.valueOf(info[0]);
+                price *= 0.99 + 0.02 * Math.random();
+                info[0] = String.format("%.5f",price);
+            }else {
+                double price = Double.valueOf(info[1]);
+                price *= 0.99 + 0.02 * Math.random();
+                info[1] = String.format("%.5f",price);
+            }
+            fileEditor.writeFile(filename,info);
         }
-        infolabel.setText(stockList.size()+" Stock Prices Have Changed");
+        infolabel.setText(infoList.size()+" "+type+" Prices Have Changed");
     }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main (String[] args){
+    public static void main (String[] args) {
         //a hard-coded account lib
         LoginData accountData= new LoginData();
         new Login(accountData);
